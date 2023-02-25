bcea8cd9bd5002b6b695dd80eeae712ee3c33bbc
DarkBum <DarkBum.Zweite@web.de>
2023-02-25 17:30:19 +0100

    Tab Item change

--- src/main/java/ru/liahim/saltmod/common/SaltTab.java
+++ src/main/java/ru/liahim/saltmod/common/SaltTab.java
@@ -4,15 +4,13 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import ru.liahim.saltmod.init.ModBlocks;
+import ru.liahim.saltmod.init.ModItems;
 
 public class SaltTab extends CreativeTabs {
-  public SaltTab(String lable) {
-    super(lable);
-  }
-  
-  @SideOnly(Side.CLIENT)
-  public Item getTabIconItem() {
-    return Item.getItemFromBlock(ModBlocks.saltOre);
-  }
+    public SaltTab(String lable) {
+        super(lable);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public Item getTabIconItem() { return ModItems.salt; }
 }
