8952ce2ed71d0ae3e73e908db488c5a914b7045f
DarkBum <DarkBum.Zweite@web.de>
2023-03-24 06:21:59 +0100

    Bat drops Strider meat now

--- src/main/java/darkbum/saltymod/network/SaltModEventHandler.java
+++ src/main/java/darkbum/saltymod/network/SaltModEventHandler.java
@@ -246,23 +246,30 @@ public class SaltModEventHandler {
         int dropAmountHorse = random.nextInt(2) + 1;
         int dropAmountSquid = random.nextInt(3) + 1;
         int dropAmountZombie = 1;
-        if (event.entityLiving instanceof net.minecraft.entity.passive.EntitySquid)
+        int dropAmountStrider = 1;
+
+        if (event.entityLiving instanceof net.minecraft.entity.monster.EntityZombie &&
+            !event.entityLiving.isChild() &&
+            ThreadLocalRandom.current().nextInt(0, 1000) < 25) {
+            event.entityLiving.entityDropItem(new ItemStack(ModItems.onion, dropAmountZombie), dropped);
+        } else if (event.entityLiving instanceof net.minecraft.entity.passive.EntitySquid) {
             if (event.entityLiving.isBurning()) {
                 event.entityLiving.entityDropItem(new ItemStack(ModItems.cooked_calamari, dropAmountSquid), dropped);
             } else {
                 event.entityLiving.entityDropItem(new ItemStack(ModItems.calamari, dropAmountSquid), dropped);
             }
-        if (event.entityLiving instanceof net.minecraft.entity.passive.EntityHorse &&
-            !event.entityLiving.isChild())
+        } else if (event.entityLiving instanceof net.minecraft.entity.passive.EntityHorse && !event.entityLiving.isChild()) {
             if (event.entityLiving.isBurning()) {
                 event.entityLiving.entityDropItem(new ItemStack(ModItems.cooked_haunch, dropAmountHorse), dropped);
             } else {
                 event.entityLiving.entityDropItem(new ItemStack(ModItems.haunch, dropAmountHorse), dropped);
             }
-        if (event.entityLiving instanceof net.minecraft.entity.monster.EntityZombie &&
-            !event.entityLiving.isChild() &&
-            ThreadLocalRandom.current().nextInt(0, 1000) < 25) {
-            event.entityLiving.entityDropItem(new ItemStack(ModItems.onion, dropAmountZombie), dropped);
+        } else if (event.entityLiving instanceof net.minecraft.entity.passive.EntityBat) {
+            if (event.entityLiving.isBurning()) {
+                event.entityLiving.entityDropItem(new ItemStack(ModItems.cooked_strider, dropAmountStrider), dropped);
+            } else {
+                event.entityLiving.entityDropItem(new ItemStack(ModItems.strider, dropAmountStrider), dropped);
+            }
         }
     }
 }
