096ec34c47a1f29315ed88d89ecc2ec8b0ec8d31
DarkBum <DarkBum.Zweite@web.de>
2023-03-29 09:32:10 +0200

    Salt Shard Play

--- src/main/java/darkbum/saltymod/item/ItemSaltShard.java
+++ src/main/java/darkbum/saltymod/item/ItemSaltShard.java
@@ -1,9 +1,18 @@
 package darkbum.saltymod.item;
 
+import darkbum.saltymod.init.AchievSalt;
+import darkbum.saltymod.init.ModItems;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.monster.EntityWitch;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
 
 public class ItemSaltShard extends Item {
 
@@ -12,7 +21,28 @@ public class ItemSaltShard extends Item {
         setCreativeTab(tab);
     }
 
-    public boolean hitEntity(ItemStack stack, EntityLivingBase p_77644_2_, EntityLivingBase p_77644_3_) {
+    public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+        World world = player.worldObj;
+        if (!player.capabilities.isCreativeMode) {
+            stack.stackSize--;
+            if (stack.stackSize == 0) {
+                player.setCurrentItemOrArmor(0, null);
+                if (EntityList.getEntityString(entity).toLowerCase().contains("slime") &&
+                    !EntityList.getEntityString(entity).toLowerCase().contains("lava")) {
+                    entity.attackEntityFrom(DamageSource.cactus, 30.0F);
+                    entity.dropItem(ModItems.tough_jelly, 1);
+                    world.playSoundEffect(entity.posX, entity.posY, entity.posZ, "dig.stone", 2.0F, 1.0F);
+                    world.playSoundEffect(entity.posX, entity.posY, entity.posZ, "dig.glass", 2.0F, 2.0F);
+                    player.addStat(AchievSalt.saltSlime, 1);
+                } if  (EntityList.getEntityString(entity).toLowerCase().contains("witch")) {
+                    entity.attackEntityFrom(DamageSource.cactus, 30.0F);
+                    entity.dropItem(ModItems.salt_pinch, 1);
+                    world.playSoundEffect(entity.posX, entity.posY, entity.posZ, "dig.stone", 2.0F, 1.0F);
+                    world.playSoundEffect(entity.posX, entity.posY, entity.posZ, "dig.glass", 2.0F, 2.0F);
+                    player.addStat(AchievSalt.saltWitch, 1);
+                }
+            }
+        }
         return false;
     }
 }
