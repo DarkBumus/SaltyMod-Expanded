577a8a2410d3891f7ea0fb046aa373a8d148ebcb
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-06-20 14:56:37 -0400

    Finish NEI Handler: every items renders in the right slot

--- src/main/java/darkbum/saltymod/api/nei/NEIPotcookingRecipeHandler.java
+++ src/main/java/darkbum/saltymod/api/nei/NEIPotcookingRecipeHandler.java
@@ -4,10 +4,13 @@ import codechicken.nei.NEIServerUtils;
 import codechicken.nei.PositionedStack;
 import codechicken.nei.recipe.TemplateRecipeHandler;
 import darkbum.saltymod.SaltyMod;
+import darkbum.saltymod.init.ModItems;
 import darkbum.saltymod.inventory.gui.GuiCookingPot;
+import darkbum.saltymod.item.ItemSalt;
 import darkbum.saltymod.util.PotcookingRecipe;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -30,16 +33,30 @@ public class NEIPotcookingRecipeHandler extends TemplateRecipeHandler {
         public CookingPotPair(PotcookingRecipe.PotRecipe recipe) {
             this.ingreds = new ArrayList<>();
             for (int i = 0; i < recipe.ingreds().size(); i++) {
-                int rX = 18 * i;
-                int rY = Math.floorDiv(rX, 54) + 1;
+                int i2 = i;
                 PotcookingRecipe.IIngredientMatcher ingred = recipe.ingreds().get(i);
+                PotcookingRecipe.IIngredientMatcher firstIngred = recipe.ingreds().get(0);
+                if(firstIngred instanceof PotcookingRecipe.StackIngredient stackIngredient) {
+                    if(stackIngredient.getStack().getItem() == ModItems.salt_pinch || stackIngredient.getStack().getItem() == ModItems.sugar_pinch) {
+                        i2 = i - 1;
+                    }
+                }
+                int rX = 18 * i2;
+                int posX = ((rX % 54) + 30) - 5;
+                int rY = Math.floorDiv(rX, 54) + 1;
+                int posY = (rY * 18) - 12;
                 if(ingred instanceof PotcookingRecipe.OreIngredient oreIngredient) {
-                    ingreds.add(new PositionedStack(oreIngredient.getOres(), (rX % 54) + 30, rY * 17));
+                    ingreds.add(new PositionedStack(oreIngredient.getOres(), posX, posY));
                 } else if(ingred instanceof PotcookingRecipe.StackIngredient stackIngredient) {
-                    ingreds.add(new PositionedStack(stackIngredient.getStack(), (rX % 54) + 30, rY * 17));
+                    if(stackIngredient.getStack().getItem() == ModItems.salt_pinch || stackIngredient.getStack().getItem() == ModItems.sugar_pinch) {
+                        ingreds.add(new PositionedStack(stackIngredient.getStack(), 3, 6));
+                    } else {
+                        ingreds.add(new PositionedStack(stackIngredient.getStack(), posX, posY));
+                    }
+  
                 }
             }
-            this.result = new PositionedStack(recipe.output(), 119-6, 21);
+            this.result = new PositionedStack(recipe.output(), 119, 15);
         }
 
         @Override
@@ -64,8 +81,13 @@ public class NEIPotcookingRecipeHandler extends TemplateRecipeHandler {
 
     @Override
     public void loadTransferRects() {
-        transferRects.add(new RecipeTransferRect(new Rectangle(120, 22, 24, 24), "pot_output"));
+        transferRects.add(new RecipeTransferRect(new Rectangle(80, 10, 24, 24), SaltyMod.MODID + ".cooking_pot"));
     }
+    
+	@Override
+	public int recipiesPerPage() {
+		return 1;
+	}
 
     @Override
     public Class<? extends GuiContainer> getGuiClass() {
@@ -77,7 +99,7 @@ public class NEIPotcookingRecipeHandler extends TemplateRecipeHandler {
      */
     @Override
     public String getGuiTexture() {
-        return "saltymod:textures/gui/container/cooking_pot.png";
+        return SaltyMod.MODID + ":textures/gui/container/cooking_pot.png";
     }
 
     /**
